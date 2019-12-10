1cb100dc48c748d2599487600b68682d79f23998
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-12-10 02:28:03 -0300

    Bubble Sort

--- bubbleSort.c
+++ bubbleSort.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
 int main(){
-    int vetor[] = {10, 3, 9, 15, 2, 0, -1};
+    int vetor[] = {10, 3, 9, 15, 2, 0, -1, 10, 91, 103, -123, 31923};
+    int numeroAcoes = 0;
+    
+    printf("\n\nTamanho do vetor: %d\n", sizeof(vetor)/sizeof(int));
+    printf("Vetor desordenado\n\n");
+    for(int i = 0; i < (sizeof(vetor) / sizeof(int)); i++){
+        printf("%d ", vetor[i]);
+    }
 
     for(int i = (sizeof(vetor) / sizeof(int)) - 1; i > 0; i--){
         for(int j = 0; j < i; j++){
+            // Calculando o número de ações
+            numeroAcoes++;
+            // Comparando o par de números
             if(vetor[j] > vetor[j+1]){
                 int temp = vetor[j];
                 vetor[j] = vetor[j+1];
@@ -13,6 +23,9 @@ int main(){
         }
     }
 
+    printf("\n\nNumero de acoes executadas: %d\n\n", numeroAcoes);
+
+    printf("Vetor ordenado\n\n");
     for(int i = 0; i < (sizeof(vetor) / sizeof(int)); i++){
         printf("%d ", vetor[i]);
     }
